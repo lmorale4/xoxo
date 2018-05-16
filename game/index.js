10b2d7099976b8ffcf0a73537325f41e5a60ac4c
Linda Morales <lindagmorales94@gmail.com>
2018-05-16 14:34:30 -0400

    up until the winners function

--- game/index.js
+++ game/index.js
@@ -1,6 +1,35 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+// Constants
+
+const DEFAULT = {
+  board: Map(),
+  turn: ' X ',
+};
+
+const MOVE = 'MOVE';
+
+// action creator
+
+export const move = (player, position) => ({
+  type: MOVE,
+  position,
+  player,
+});
+
+export const winner = (board, position, turn) => {
+  const [row, col] = position;
+  if(board.getIn([row+1, col+ 1], turn))
+};
+
+export default function reducer(state = DEFAULT, action) {
+  switch (action.type) {
+    case MOVE:
+      return {
+        board: state.board.setIn(action.position, action.player),
+        turn: state.turn === ' X ' ? ' O ' : ' X ',
+      };
+    default:
+      return state;
+  }
 }
